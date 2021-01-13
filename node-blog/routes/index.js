672e60234a60ae2c4585e3111a48f7d5841975b2
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2021-01-13 22:02:01 +0530

    ok

--- node-blog/routes/index.js
+++ node-blog/routes/index.js
@@ -14,6 +14,40 @@ router.get('/create',function (req,res,next) {
   res.render('create', { title: 'Create a new blog' });
 
 });
+router.get('/blogs',function (req,res,next) {
+  Blog.find().sort({ createdAt: -1 })
+      .then(result => {
+        res.render('index', { blogs: result, title: 'All blogs' });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+
+});
+
+router.get('/blogs/:id',function (req,res,next) {
+  const id = req.params.id;
+  Blog.findById(id)
+      .then(result => {
+        res.render('details', { blog: result, title: 'Blog Details' });
+      })
+      .catch(err => {
+        console.log(err);
+        res.render('error', { title: 'Blog not found' });
+      });
+
+});
+router.delete('/blogs/:id',function (req,res,next) {
+  const id = req.params.id;
+  Blog.findByIdAndDelete(id)
+      .then(result => {
+        res.json({ redirect: '/blogs' });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+
+});
 
 router.post('/blogs',function (req,res) {
   const blog = new Blog(req.body);
