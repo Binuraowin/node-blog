6baabb00775825586d2cef0e2c229f6cbedea2ff
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2021-01-13 20:50:11 +0530

    database

--- node-blog/routes/index.js
+++ node-blog/routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var Blog = require('../models/blog');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,5 +9,23 @@ router.get('/', function(req, res, next) {
 router.get('/about', function(req, res, next) {
   res.render('about', { title: 'About' });
 });
+
+router.get('/create',function (req,res,next) {
+  res.render('create', { title: 'Create a new blog' });
+
+});
+
+router.post('/blogs',function (req,res) {
+  const blog = new Blog(req.body);
+  console.log(req.body)
+  blog.save().then(
+      result => {
+        res.redirect('/blogs');
+      }
+  ).catch(err =>{
+    console.log(err);
+  })
+
+})
 
 module.exports = router;
